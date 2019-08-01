518040d9e25e0774021aeb456c8caecb6475e033
phplessons2018 <s.dimitrenco@webdelo.org>
2019-08-01 22:36:10 +0300

    Spring Boot Jpa (Hibernate): добавляем связи между таблицами базы данных (one to many)

--- src/main/java/com/spring2/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/spring2/sweater/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring2.sweater.config;
 
+import com.spring2.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,8 +14,10 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
-    private DataSource dataSource;
+    private UserService userService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -32,11 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("select username, password, active from user where username=?")
-                .authoritiesByUsernameQuery("select u.username, ur.roles from user u inner join user_role ur on u.id = ur.user_id where u.username=?");
+        auth.userDetailsService(userService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
 
     }
 }
